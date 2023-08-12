bfc5a05ce7f2ff9a6ccd096ac7756f9cd733effb
Donatus Herre <donatus.herre@slub-dresden.de>
2023-08-12 13:13:34 +0200

    Update sort in Find.url_query

--- txpyfind/client.py
+++ txpyfind/client.py
@@ -88,7 +88,7 @@ class Find:
                 self.logger.warning("Count exceeds limit!")
                 count = self.count_limit
             url = utils.add_tx_param(url, "count", count)
-        if sort != "" and (sort_pattern is None or not isinstance(sort_pattern.match(sort), re.Match)):
+        if sort != "" and sort_pattern is not None and not isinstance(sort_pattern.match(sort), re.Match):
             self.logger.warning("Unknown sort instruction!")
             sort = ""
         if sort != "":
