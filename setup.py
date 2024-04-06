de20bd97b72531f0416b287236de9237f0388e04
Donatus Herre <donatus.herre@slub-dresden.de>
2024-04-07 00:38:35 +0200

    Update description in setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version="0.0.0",
     author="Donatus Herre",
     author_email="donatus.herre@slub-dresden.de",
-    description="Export data via TYPO3-find.",
+    description="This package enables access to data exports from TYPO3-find.",
     long_description=open("README.rst").read(),
     long_description_content_type="text/x-rst",
     license="GPLv3",
