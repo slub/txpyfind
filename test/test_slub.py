c12859ca49cc291d1c6923654b8b949befd76945
Donatus Herre <donatus.herre@slub-dresden.de>
2024-11-14 22:21:26 +0100

    add another test

--- test/test_slub.py
+++ test/test_slub.py
@@ -21,6 +21,16 @@ def test_get_query():
    assert any(slub_ld_doc.raw['@graph'])
    assert '@id' in slub_ld_doc.raw['@graph'][0]
 
+def test_get_query_via_url():
+   # create Find instance
+   slub_find = Find("https://katalog.slub-dresden.de", document_path="id", export_format="json-ld")
+   # retrieve JSON-LD data (query view)
+   slub_ld_doc = slub_find.get_query_via_url("https://katalog.slub-dresden.de/?tx_find_find%5Bq%5D%5Bdefault%5D=manfred+bonitz")
+   assert slub_ld_doc is not None
+   assert '@graph' in slub_ld_doc.raw
+   assert any(slub_ld_doc.raw['@graph'])
+   assert '@id' in slub_ld_doc.raw['@graph'][0]
+
 def test_pagination():
    # create Find instance
    slub_find = Find("https://katalog.slub-dresden.de", document_path="id")
